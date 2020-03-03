e42544e35b5c05793882ef0709da5d0bccbcb4b1
Nick Spinelli <nickspinelli85@gmail.com>
2020-03-02 21:36:38 -0500

    Setting up registration

---
+++ client/src/components/Register.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <h2>Another Heading</h2>
+    <input type="email" name="email" placeholder="email" />
+    <br />
+    <input type="password" name="password" placeholder="password" />
+    <br />
+    <button></button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Register extends Vue {
+  data() {
+    return {};
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
---
+++ client/src/views/Register.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" name="email" v-model="email" placeholder="email" />
+    <br />
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password"
+    />
+    <br />
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import AuthenticationService from "@/services/AuthenticationService";
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  watch: {
+    email(value) {
+      console.log("email has changed", value);
+    }
+  },
+  methods: {
+    async register() {
+    const response = await AuthenticationService.register({
+      email: this.email,
+      password: this.password
+    })
+    console.log(response)
+    }
+  },
+  name: "Home",
+  components: {}
+};
+</script>
